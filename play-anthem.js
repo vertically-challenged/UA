91129b37a5a1bf5b2e8db65ab3fdc5e782aa3d9b
vertically-challenged <50969768+vertically-challenged@users.noreply.github.com>
2019-06-07 15:15:33 +0300

    final fixes js

--- play-anthem.js
+++ play-anthem.js
@@ -1,8 +1,72 @@
-var audio = $("#sound")[0];
+var audio1 = $("#sound01")[0];
 $("#sound1").mouseenter(function() {
-  audio.play();
+  audio1.play();
 }).mouseleave(function() {
-  audio.pause();
+  audio1.pause();
 });
 
-// Хз почему не работает, я ни бум-бум в js
+var audio2 = $("#sound02")[0];
+$("#sound2").mouseenter(function() {
+  audio2.play();
+}).mouseleave(function() {
+  audio2.pause();
+});
+
+var audio3 = $("#sound03")[0];
+$("#sound3").mouseenter(function() {
+  audio3.play();
+}).mouseleave(function() {
+  audio3.pause();
+});
+
+var audio4 = $("#sound04")[0];
+$("#sound4").mouseenter(function() {
+  audio4.play();
+}).mouseleave(function() {
+  audio4.pause();
+});
+
+var audio5 = $("#sound05")[0];
+$("#sound5").mouseenter(function() {
+  audio5.play();
+}).mouseleave(function() {
+  audio5.pause();
+});
+
+var audio6 = $("#sound06")[0];
+$("#sound6").mouseenter(function() {
+  audio6.play();
+}).mouseleave(function() {
+  audio6.pause();
+});
+
+var audio7 = $("#sound07")[0];
+$("#sound7").mouseenter(function() {
+  audio7.play();
+}).mouseleave(function() {
+  audio7.pause();
+});
+
+var audio8 = $("#sound08")[0];
+$("#sound8").mouseenter(function() {
+  audio8.play();
+}).mouseleave(function() {
+  audio8.pause();
+});
+
+var audio1=document.querySelector("#sound01");
+audio1.volume=0.15;
+var audio2=document.querySelector("#sound02");
+audio2.volume=0.15;
+var audio3=document.querySelector("#sound03");
+audio3.volume=0.15;
+var audio4=document.querySelector("#sound04");
+audio4.volume=0.15;
+var audio5=document.querySelector("#sound05");
+audio5.volume=0.15;
+var audio6=document.querySelector("#sound06");
+audio6.volume=0.15;
+var audio7=document.querySelector("#sound07");
+audio7.volume=0.15;
+var audio8=document.querySelector("#sound08");
+audio8.volume=0.15;
