287e2b04ebfd20f0f6bf72bb08385a0046a8417f
vertically-challenged <anton2chewski@gmail.com>
2019-12-31 12:46:32 +0300

    update script

--- play-anthem.js
+++ play-anthem.js
@@ -1,72 +1,26 @@
-var audio1 = $("#sound01")[0];
-$("#sound1").mouseenter(function() {
-  audio1.play();
-}).mouseleave(function() {
-  audio1.pause();
-});
+let audio = {
+  1: $("#sound01")[0],
+  2: $("#sound02")[0],
+  3: $("#sound03")[0],
+  4: $("#sound04")[0],
+  5: $("#sound05")[0],
+  6: $("#sound06")[0],
+  7: $("#sound07")[0],
+  8: $("#sound08")[0],
+  9: $("#sound09")[0],
+}
 
-var audio2 = $("#sound02")[0];
-$("#sound2").mouseenter(function() {
-  audio2.play();
-}).mouseleave(function() {
-  audio2.pause();
-});
+for (let k = 1; k < 9; k++) {
+  $("#sound" + k).mouseenter(function() {
+    audio[k].play();
+   }).mouseleave(function() {
+    audio[k].pause();
+  }); 
+}
 
-var audio3 = $("#sound03")[0];
-$("#sound3").mouseenter(function() {
-  audio3.play();
-}).mouseleave(function() {
-  audio3.pause();
-});
-
-var audio4 = $("#sound04")[0];
-$("#sound4").mouseenter(function() {
-  audio4.play();
-}).mouseleave(function() {
-  audio4.pause();
-});
-
-var audio5 = $("#sound05")[0];
-$("#sound5").mouseenter(function() {
-  audio5.play();
-}).mouseleave(function() {
-  audio5.pause();
-});
-
-var audio6 = $("#sound06")[0];
-$("#sound6").mouseenter(function() {
-  audio6.play();
-}).mouseleave(function() {
-  audio6.pause();
-});
-
-var audio7 = $("#sound07")[0];
-$("#sound7").mouseenter(function() {
-  audio7.play();
-}).mouseleave(function() {
-  audio7.pause();
-});
-
-var audio8 = $("#sound08")[0];
-$("#sound8").mouseenter(function() {
-  audio8.play();
-}).mouseleave(function() {
-  audio8.pause();
-});
-
-var audio1=document.querySelector("#sound01");
-audio1.volume=0.15;
-var audio2=document.querySelector("#sound02");
-audio2.volume=0.15;
-var audio3=document.querySelector("#sound03");
-audio3.volume=0.15;
-var audio4=document.querySelector("#sound04");
-audio4.volume=0.15;
-var audio5=document.querySelector("#sound05");
-audio5.volume=0.15;
-var audio6=document.querySelector("#sound06");
-audio6.volume=0.15;
-var audio7=document.querySelector("#sound07");
-audio7.volume=0.15;
-var audio8=document.querySelector("#sound08");
-audio8.volume=0.15;
+let i = 1; 
+for (let key in audio) {
+  audio[key] = document.querySelector("#sound0" + i);
+  audio[key].volume = 0.15; 
+  i++; 
+}
